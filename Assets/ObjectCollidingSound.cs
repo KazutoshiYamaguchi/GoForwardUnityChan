79ef941d01d5831142b663339e28bd57dd23dda9
KazutoshiYamaguchi <umaibou300pon@icloud.com>
2020-07-27 17:16:21 +0900

    GoForwardUnityChan 2Dゲームアプリ作成
    キューブ同士の接触時に効果音をつけました。

---
+++ Assets/ObjectCollidingSound.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class ObjectCollidingSound : MonoBehaviour
+{
+
+    // Use this for initialization
+
+
+    public AudioClip impact;
+    AudioSource audioSource;
+    GameObject unitychan;
+
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        unitychan = GameObject.Find("UnityChan2D");
+    }
+
+    void Update()
+    {
+       
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == unitychan)
+        {
+            Debug.Log("Accidentallly Hit! But,NoSound.");
+        }
+        else
+        {
+           audioSource.PlayOneShot(impact);
+        }
+        
+    }
+
+}
